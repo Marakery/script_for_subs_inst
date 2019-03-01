f9e0ab2bff58c44caa2943a089778b5e291b5a51
Mark <marcus.koh16.01.97@gmail.com>
2019-03-02 00:38:17 +0300

    Add files via upload

--- url.php
+++ url.php
@@ -1,32 +1,38 @@
 <?php
     // создание нового ресурса cURL
-$counter = 0;
-for ($x = 2332944552; $x <= 3332944552; $x++) {
-    
-    
-    $curl = curl_init();
-            $url = "https://followmania.vip/GetUserInfo?postTag=c2VsYW1rYW5rYWJ1YmlyYmFzZTY0&device_id=$x";
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_ENCODING, "gzip");
-            curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, '{"device_id": "$x"}');
+    // 9479926142 , 10702006028 , 
+    $x = 0;
+        do {
 
-            $ex = curl_exec($curl); 
-            $str = "[]";
-            curl_close($curl);
-       
-            if (strcasecmp($ex, $str) != 0) {
-                $counter = $counter + 1;
-                $fp = fopen('text.txt', 'a');
-                fwrite($fp, $ex);
-                fwrite($fp, "\r\n");   
-                fclose($fp);
-                echo $ex;
-            }
-            
-}
-echo "Goodstuff $counter \n";
 
+        $i = 0;
+        do {
+            echo "Out:  ";
+            $curl = curl_init();
+                    $id = (string)11308009154;
+                    $url = "https://followmania.vip/Send?postTag=c2VsYW1rYW5rYWJ1YmlyYmFzZTY0&device_id=$id";
+                    curl_setopt($curl, CURLOPT_URL, $url);
+                    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($curl, CURLOPT_ENCODING, "gzip");
+                    curl_setopt($curl, CURLOPT_POST, 1);
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, '{"device_id": 11308009154, "username":"uraestheticgirls", "type":"followers","followers_count":"20","credi_count":"2"}');
+            $curl2 = curl_init();
+                    curl_setopt($curl2, CURLOPT_URL,"https://followmania.vip/GetUserInfo?postTag=c2VsYW1rYW5rYWJ1YmlyYmFzZTY0&device_id=$id");
+                    $ef = curl_exec($curl2);
+                    $ex = curl_exec($curl); 
+                    curl_close($curl);
+                    curl_close($curl2);
+                    echo $ex;
+                    echo $ef;
+                    echo "\n";
+                    sleep(10); 
+                    $i = $i + 1;    
+        } while ($i <= 25);
+        
+        echo "sleep \n";
+        sleep(1800);
+        $i = 0;  
+        $x = $x + 1;
+    } while ($x <= 50);
 ?>
