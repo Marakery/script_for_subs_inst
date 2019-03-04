905aa12c53ca66a2f99d615f0febf8c73ad60797
Mark <marcus.koh16.01.97@gmail.com>
2019-03-05 02:24:43 +0300

    Add files via upload

--- u.py
+++ u.py
@@ -1,33 +1,38 @@
 import requests
 import time
+import random
 
 
-device_id = '10584148734'
-username = 'mrak91'
+
+device_id = '8560953769'
+username = 'andreivv'
 url = 'https://followmania.vip/Send?postTag=c2VsYW1rYW5rYWJ1YmlyYmFzZTY0&device_id=' + device_id
 url_get = 'https://followmania.vip/GetUserInfo?postTag=c2VsYW1rYW5rYWJ1YmlyYmFzZTY0&device_id=' + device_id
 
 payload_info = {"device_id": device_id}
 followers = 'followers'
-followers_count = '20'
+followers_count = '1'
 credi_count = '2'
 
-for i in range(0, 25):
-    user_info = requests.get(url_get)
-    
-    resp = requests.post(
-        url,
-        json=dict(
-                  device_id=device_id, 
-                  username=username, 
-                  type=followers, 
-                  followers_count=followers_count, 
-                  credi_count=credi_count
-                  ),
-        #verify=False,
-    )
-    data = resp.text
-    # or if you expect json response
-    data = resp.json()
-    print(username,' ', user_info.text,' : ', data) 
-    time.sleep(10)
+for x in range(0, 500):
+    for i in range(0, 25):
+        random_subs_value = random.randint(1,15)
+        user_info = requests.get(url_get)
+ 
+        resp = requests.post(
+            url,
+            json=dict(
+                      device_id=device_id, 
+                      username=username, 
+                      type=followers, 
+                      followers_count=random_subs_value, 
+                      credi_count=credi_count
+                      ),
+            #verify=False,
+        )
+        data = resp.text
+        # or if you expect json response
+        data = resp.json()
+        print(username,' ', user_info.text,' : ', data) 
+        time.sleep(10)
+    time.sleep(1800)
